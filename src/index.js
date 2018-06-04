0f0a1474982f4d97afb7722a65884c1764c56d89
Bruno Michel <bmichel@menfin.info>
2018-06-04 10:36:38 +0200

    Add signin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+// XXX The default HTTP parser of Node.js (in C++) is quite strict and does not
+// accept responses with 2 CONTENT_LENGTH headers (even with the same value).
+// The SOS Villages d'Enfants website does this on some response. A work-around is
+// to use http-parser-js, but to work it must be loaded before any
+// `require('http')`. So, we put it in the first line here. But it's not enough for
+// `yarn standalone` that loads some libs before loading this file. If you have a
+// `HPE_UNEXPECTED_CONTENT_LENGTH` error when running it in development, you can
+// copy the line below to `node_modules/.bin/cozy-run-standalone`.
+process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser
+
 const {
   BaseKonnector,
   requestFactory,
@@ -7,74 +17,52 @@ const {
   log
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very usefull for
-  // debugging but very verbose. That is why it is commented out by default
-  // debug: true,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
+  // debug: false,
   cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
   json: false,
-  // this allows request-promise to keep cookies between requests
   jar: true
 })
 
-const baseUrl = 'http://books.toscrape.com'
+const baseUrl = 'https://www.sosve.org/espace-donateur'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.login, fields.password)
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
   log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
+  const $ = await request(`${baseUrl}/dons-et-recus-fiscaux/`)
   log('info', 'Parsing list of documents')
   const documents = await parseDocuments($)
 
-  // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // common case in connectors
   log('info', 'Saving data to Cozy')
   await saveBills(documents, fields.folderPath, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
+    identifiers: ['SOSVE', 'SOS VILLAGES ENFANTS']
   })
 }
 
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
 function authenticate(username, password) {
   return signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // the validate function will check if
+    url: `${baseUrl}/`,
+    formSelector: '#loginform',
+    formData: {
+      log: username,
+      pwd: password
+    },
     validate: (statusCode, $) => {
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
+      if ($(`a[href='${baseUrl}/mon-profil/mot-de-passe/']`).length === 1) {
         return true
       } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
         log('error', $('.error').text())
         return false
       }
-    }
+    },
+    requestOpts: {}
   })
 }
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
 function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
   const docs = scrape(
     $,
     {
@@ -106,16 +94,11 @@ function parseDocuments($) {
   )
   return docs.map(doc => ({
     ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
     date: new Date(),
     currency: '€',
-    vendor: 'template',
+    vendor: 'SOSVE',
     metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // usefull for debugging or data migration
       importDate: new Date(),
-      // document version, usefull for migration after change of document structure
       version: 1
     }
   }))
